e417f2849ffe79d19b471170649f363744fcff53
haipro287 <haipro287@gmail.com>
2021-09-27 22:40:11 +0700

    add component and scss

---
+++ src/components/appbar/Appbar.tsx
@@ -0,0 +1,9 @@
+import styles from './Appbar.module.scss'
+
+export default function Appbar() {
+  return (
+    <header className={styles.header}>
+      <div className={styles.title}>YUMEMI FE</div>
+    </header>
+  )
+}
---
+++ src/components/layout/Layout.tsx
@@ -0,0 +1,19 @@
+import Head from 'next/head'
+import Appbar from '@components/appbar/Appbar'
+import Footer from '@components/footer/Footer'
+import styles from './Layout.module.scss'
+
+export default function Layout({ children }) {
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title>Yumemi FE</title>
+        <meta name='description' content='Powered by haipro287' />
+        <link rel='icon' href='/favicon.ico' />
+      </Head>
+      <Appbar />
+      <main className={styles.main}>{children}</main>
+      <Footer />
+    </div>
+  )
+}
