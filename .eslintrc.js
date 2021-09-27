5ba027216f024b2d282a77aee101d20a59f26710
haipro287 <haipro287@gmail.com>
2021-09-27 13:42:59 +0700

    fix eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -159,6 +159,7 @@ module.exports = {
         "react/jsx-props-no-spreading": 0,
         "react/prop-types": 0,
         "@typescript-eslint/no-unsafe-call": 0,
+        "@typescript-eslint/no-unsafe-return": 0,
         "import/no-extraneous-dependencies": 0,
     },
     plugins: ["html", "prettier", "react-hooks", "@typescript-eslint"],
--- src/pages/api/prefectures.ts
+++ src/pages/api/prefectures.ts
@@ -1,20 +1,20 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
-import { resaxios } from "@utils/custom-axios";
+import { resaxios } from '@utils/custom-axios'
 
-const axios = resaxios;
+const axios = resaxios
 
 export default async function handler(req, res) {
-    await axios
-        .get("/prefectures")
-        .then(r => {
-            res.status(200).json(r.data);
-        })
-        .catch(err => {
-            res.status(200).json({
-                description: "",
-                message: "Internal.",
-                statusCode: "500",
-            });
-        });
+  await axios
+    .get('/prefectures')
+    .then(r => {
+      res.status(200).json(r.data)
+    })
+    .catch(err => {
+      res.status(200).json({
+        description: '',
+        message: 'Internal.',
+        statusCode: '500',
+      })
+    })
 }
