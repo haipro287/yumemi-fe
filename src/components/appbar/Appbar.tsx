fee47f2a7fc9965ee64e6f807039d2ed3840101f
haipro287 <haipro287@gmail.com>
2021-09-28 12:50:35 +0700

    fix responsive css

--- src/components/appbar/Appbar.tsx
+++ src/components/appbar/Appbar.tsx
@@ -2,8 +2,8 @@ import styles from './Appbar.module.scss'
 
 export default function Appbar() {
   return (
-    <header className={styles.header}>
+    <div className={styles.header}>
       <div className={styles.title}>YUMEMI FE</div>
-    </header>
+    </div>
   )
 }
