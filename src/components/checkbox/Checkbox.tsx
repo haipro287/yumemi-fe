57fb69a73967be8aad7fd9ecdd0310adea28e151
haipro287 <haipro287@gmail.com>
2021-09-29 06:45:56 +0700

    update css

--- src/components/checkbox/Checkbox.tsx
+++ src/components/checkbox/Checkbox.tsx
@@ -1,26 +1,27 @@
-import styles from "./Checkbox.module.scss";
-import { Prefecture } from "../../pages";
+import styles from './Checkbox.module.scss'
+import { Prefecture } from '../../pages'
 
 interface Props {
-    id: number;
-    p: Prefecture;
-    handle: (value: any) => void;
+  id: number
+  pref: Prefecture
+  handle: (value: any) => void
 }
 
 export default function Checkbox(props: Props) {
-    return (
-        <div className={styles.checkbox}>
-            <input
-                id={props.p.prefCode.toString()}
-                className={styles.styledCheckbox}
-                type="checkbox"
-                onChange={props.handle}
-                value={props.id}
-            />
+  const { id, pref, handle } = props
+  return (
+    <div className={styles.checkbox}>
+      <input
+        id={pref.prefCode.toString()}
+        className={styles.styledCheckbox}
+        type='checkbox'
+        onChange={handle}
+        value={id}
+      />
 
-            <label htmlFor={props.p.prefCode.toString()}>
-                <span></span> {props.p.prefName}
-            </label>
-        </div>
-    );
+      <label htmlFor={pref.prefCode.toString()}>
+        <span /> {pref.prefName}
+      </label>
+    </div>
+  )
 }
